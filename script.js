52aba493720e7d9fd268f8d8d7cd5238b178d19c
Daniel Egidio <danielegidioti@gmail.com>
2021-03-03 17:48:09 -0300

    Procura pelo candidato e voto nulo

--- script.js
+++ script.js
@@ -28,18 +28,46 @@ function comecarEtapa(){
     lateral.innerHTML = '';
     numeros.innerHTML = numeroHtml; 
 }
-function atualizaInterface(){
 
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+        let fotosHtml = '';
+        for(let i in candidato.fotos) {
+            if(candidato.fotos[i].small) {
+                fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            } else {
+                fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            }
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
 }
 
 function clicou(n) {
     let elNumero = document.querySelector('.numero.pisca');
     if(elNumero !== null) {
-        elNumero.innerHTML=n;
+        elNumero.innerHTML = n;
         numero = `${numero}${n}`;
 
         elNumero.classList.remove('pisca');
-        if(nextElementSibling !== null){
+        if(elNumero.nextElementSibling !== null) {
             elNumero.nextElementSibling.classList.add('pisca');
         } else {
             atualizaInterface();
